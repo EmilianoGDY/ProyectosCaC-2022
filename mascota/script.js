45ccea3e8699c72cbdda9560f4eb5c7b20369b01
EmilianoGDY <emilianoo9g@gmail.com>
2022-10-04 18:25:18 -0300

    alimentar

--- mascota/script.js
+++ mascota/script.js
@@ -15,15 +15,20 @@ const perro = new Animal(`Perro`, `Lio`, 2);
 
 const boton = document.querySelector(".botoncito");
 
-
+const alimento = document.querySelector(".alimentar")
 
 boton.addEventListener("click", cambioTexto);
 
+alimento.addEventListener("click", alerta);
+
 function cambioTexto(){
     const texto = document.getElementById("textito");
-    texto.innerHTML = `Hola a todos, Me llamo ${perro.nombre}, soy un ${perro.especie} pincher y tengo ${perro.edad} años. Nací en Catamarca pero vivo en Corrienets con mi familia. Me gusta mucho jugar a la pelota y comer pollo hervido`;
+    texto.innerHTML = `Hola a todos, Me llamo ${perro.nombre}, soy un ${perro.especie} pincher y tengo ${perro.edad} años. Nací en Ca5tamarca pero vivo en Corrienets con mi familia. Me gusta mucho jugar a la pelota y comer pollo hervido`;
 };
 
+function alerta(){
+    alert(`Gracias por la comida Humano!!!`);
+}
 
 
 
