a1794b0fcd056d683af040a96f5aea01193275f4
EmilianoGDY <emilianoo9g@gmail.com>
2022-10-04 18:06:52 -0300

    presentacion terminada

--- mascota/script.js
+++ mascota/script.js
@@ -13,10 +13,29 @@ class Animal{
 
 const perro = new Animal(`Perro`, `Lio`, 2);
 
-console.log(perro);
+const boton = document.querySelector(".botoncito");
+
+
+
+boton.addEventListener("click", cambioTexto);
+
+function cambioTexto(){
+    const texto = document.getElementById("textito");
+    texto.innerHTML = `Hola a todos, Me llamo ${perro.nombre}, soy un ${perro.especie} pincher y tengo ${perro.edad} años. Nací en Catamarca pero vivo en Corrienets con mi familia. Me gusta mucho jugar a la pelota y comer pollo hervido`;
+};
+
+
+
+
+
+
+
+
+
+
+
+
 
 // contenedor.innerHTML = ` <p> Especie: ${perro.especie}</p>
 //                          <p> Nombre: ${perro.nombre} </p>
 //                          <p> Edad: ${perro.edad}</p>`;
-
-                       
