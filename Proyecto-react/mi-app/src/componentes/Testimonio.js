394287b15303661ccffb0f92ede4d989c90fa5d6
EmilianoGDY <emilianoo9g@gmail.com>
2022-10-26 17:59:02 -0300

    Terminados testimonios

--- Proyecto-react/mi-app/src/componentes/Testimonio.js
+++ Proyecto-react/mi-app/src/componentes/Testimonio.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import '../hojas-de-estilos/Testimonio.css';
 
-function Testimonio() {
+function Testimonio(props) {
   return(
     <div className='contenedor-testimonio'>
       <img className='imagen-testimonio' 
-      src={require('../imagenes/testimonio-emma.png')} 
+      src={require(`../imagenes/testimonio-${props.imagen}.png`)} 
       alt='foto de emma'/>
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>Emma Bostian en Suecia</p>
-        <p className='cargo-testimonio'>Ingeniera de Software en Spotify</p>
-        <p className='texto-testimonio'>"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."</p>
+        <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} en  {props.empresa} </p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
       </div>
     </div>
     );
 }
 
 
-export default Testimonio;
+export default Testimonio; 
