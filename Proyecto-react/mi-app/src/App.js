7453d4a381e989b5768858221766f2e757d6ed3f
EmilianoGDY <emilianoo9g@gmail.com>
2022-10-27 11:34:01 -0300

    Proyecto Terminado

--- Proyecto-react/mi-app/src/App.js
+++ Proyecto-react/mi-app/src/App.js
@@ -8,15 +8,6 @@ function App() {
       <div className='contenedor-principal'>
         <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
         <Testimonio
-        nombre='Emma Bostian'
-        pais='Suecia'
-        imagen='emma'
-        cargo='Ingeniera de Software'
-        empresa='Spotify'
-        testimonio='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify'
-        />
-
-        <Testimonio
         nombre='Shawn Wang'
         pais='Singapur'
         imagen='shawn'
@@ -32,6 +23,15 @@ function App() {
         cargo='Ingeniera de Software'
         empresa='ChatDesk'
         testimonio='freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.'
+        />
+
+        <Testimonio
+        nombre='Emma Bostian'
+        pais='Suecia'
+        imagen='emma'
+        cargo='Ingeniera de Software'
+        empresa='Spotify'
+        testimonio='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify'
         />
       </div>
     </div>
